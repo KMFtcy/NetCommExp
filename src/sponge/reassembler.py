7f8606a2e0d81c6407e96ce2448d4f904aa950d6
kmftcy <kmftangchaoyang@gmail.com>
2025-03-18 16:22:07 +0800

    add reassambler performance test

--- src/sponge/reassembler.py
+++ src/sponge/reassembler.py
@@ -2,10 +2,10 @@ from src.sponge.byte_stream import ByteStream
 
 # Reassembler is the upstream of ByteStream
 class Reassembler:
-    def __init__(self, stream: ByteStream):
+    def __init__(self, capacity: int):
         self.unassembled_bytes = {}
         self.first_unassembled = 0
-        self.byte_stream = stream
+        self.output = ByteStream(capacity)
 
     def insert(self, index: int, data: bytes, eof: bool) -> None:
         if index != self.first_unassembled:
@@ -14,8 +14,8 @@ class Reassembler:
         self.first_unassembled = index + len(data)
         
         # push data to byte stream
-        self.byte_stream.push(data)
+        self.output.push(data)
         
         # if eof, close byte stream
         if eof:
-            self.byte_stream.close()
+            self.output.close()
--- tests/test_reassembler.py
+++ tests/test_reassembler.py
@@ -1,36 +1,42 @@
 import unittest
 from src.sponge.byte_stream import ByteStream
 from src.sponge.reassembler import Reassembler
-
+import time
 class TestReassembler(unittest.TestCase):
     def setUp(self):
-        self.stream = ByteStream(capacity=100)
-        self.reassembler = Reassembler(self.stream)
+        self.reassembler = Reassembler(capacity=1000000)
 
     def test_insert_in_order(self):
         data = b"hello"
         self.reassembler.insert(0, data, False)
-        self.assertEqual(self.stream.bytes_buffered(), len(data))
-        self.assertEqual(self.stream.pop(len(data)), data)
+        self.assertEqual(self.reassembler.output.bytes_buffered(), len(data))
+        self.assertEqual(self.reassembler.output.pop(len(data)), data)
 
     def test_insert_out_of_order(self):
         data1 = b"world"
         data2 = b"hello"
         self.reassembler.insert(5, data1, False)  # Out of order
-        self.assertEqual(self.stream.bytes_buffered(), 0)  # Should not be added
+        self.assertEqual(self.reassembler.output.bytes_buffered(), 0)  # Should not be added
         self.reassembler.insert(0, data2, False)  # In order
-        self.assertEqual(self.stream.bytes_buffered(), len(data2))
-        self.assertEqual(self.stream.pop(len(data2)), data2)
+        self.assertEqual(self.reassembler.output.bytes_buffered(), len(data2))
+        self.assertEqual(self.reassembler.output.pop(len(data2)), data2)
         self.reassembler.insert(5, data1, False)  # Now in order
-        self.assertEqual(self.stream.bytes_buffered(), len(data1))
-        self.assertEqual(self.stream.pop(len(data1)), data1)
+        self.assertEqual(self.reassembler.output.bytes_buffered(), len(data1))
+        self.assertEqual(self.reassembler.output.pop(len(data1)), data1)
 
     def test_insert_with_eof(self):
         data = b"hello"
         self.reassembler.insert(0, data, True)
-        self.assertTrue(self.stream.is_closed())
-        self.assertEqual(self.stream.bytes_buffered(), len(data))
-        self.assertEqual(self.stream.pop(len(data)), data)
+        self.assertTrue(self.reassembler.output.is_closed())
+        self.assertEqual(self.reassembler.output.bytes_buffered(), len(data))
+        self.assertEqual(self.reassembler.output.pop(len(data)), data)
+
+    def test_insert_performance(self):
+        data = b"hello" * 100
+        start_time = time.time()
+        self.reassembler.insert(0, data, False)
+        end_time = time.time()
+        print(f"Insert bandwidth: {len(data) * 8 / (end_time - start_time) / 1e6} MBits/second")
 
 if __name__ == "__main__":
     unittest.main() 
